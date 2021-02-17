b6f0c68e747b99728c73e1ae895ff125f53e8c42
benlyazid <benlyazidbest@gmail.com>
2021-02-17 21:28:11 +0100

    creat the algo

--- main.py
+++ main.py
@@ -4,7 +4,31 @@ import random as rd
 import matplotlib.pyplot as plt
 import math
 
+def update_center_of_k_points(data_lst, k_center):
+	new_points = []
+	for i in range(len(data_lst)):
+		center = data_lst[i]
+		#for _, values in center.iterrows():
+		average_a, average_b = 0, 0
+		j = 0
+		for _ in range(len(center)):
+			values = center[j]
+			v_a = values[0]
+			v_b = values[1]
+			average_a += v_a
+			average_b += v_b
+			j += 1
+		average_a /= j
+		average_b /= j	
+		new_points.append([average_a, average_b])
+	i  = 0  
+	for _, k_point in k_center.iterrows():
+		k_point['a'] = new_points[i][0]
+		k_point['b'] = new_points[i][1]
+		i += 1
 
+
+	return k_center
 def get_the_closest_center(av, bv, centers):
 	min_dest = -1
 	result_index = 0
@@ -29,22 +53,33 @@ def assign_points_to_the_closeest_center():
 		av = row['a']
 		bv = row['b']
 		close_one = get_the_closest_center(av, bv, k_center)
-		print(close_one)
-		data_lst[close_one].append(row)
-	print('-------------------------------------\n\n')
-	print(data_lst[0])
-	print('-------------------------------------\n\n')
-	print(data_lst[1])
-	print('-------------------------------------\n\n')
-	print(data_lst[2])
+		data_lst[close_one].append([av, bv])
+	k_center = update_center_of_k_points(data_lst, k_center)
+
+def average_of_changing(old, new):
+	print(new['a'])
+	return 0
 
 data =  pd.read_csv('simpleData.csv')
 K = 3
 k_center = data.sample(n=K)
-assign_points_to_the_closeest_center()
+for _, k_point in k_center.iterrows():
+	print(k_point['a'], k_point['b'])
+
+print('AFTER')
+print('end of check')
+reapet = 0
+
+while reapet == 0 or average_of_changing(old, k_center)  5:
+	old = k_center
+	reapet += 1
+	assign_points_to_the_closeest_center()
+
+for _, k_point in k_center.iterrows():
+	print(k_point['a'], k_point['b'])
 #	Visualize The Data
 plt.scatter(data["a"], data["b"],c='red')
 plt.scatter(k_center['a'], k_center['b'], c='blue')
 plt.xlabel("a")
 plt.ylabel('b')
-plt.show()
+#plt.show()
